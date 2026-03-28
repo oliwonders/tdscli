78df1aa545ef4c82a6de9e6de807c0c2241a8120
David Oliver <david@tenthirty.net>
2026-03-28 08:38:05 -0700

    Initial commit

---
+++ Package.swift
@@ -0,0 +1,24 @@
+// swift-tools-version: 6.1
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "tdscli2",
+    platforms: [
+        .macOS(.v14,)
+    ],
+    dependencies: [
+        .package(url: "https://github.com/oliwonders/FreeTDSKit.git", branch: "main")
+    ],
+    targets: [
+        // Targets are the basic building blocks of a package, defining a module or a test suite.
+        // Targets can depend on other targets in this package and products from dependencies.
+        .executableTarget(
+            name: "tdscli2",
+            dependencies: [
+                .product(name: "FreeTDSKit", package: "FreeTDSKit")
+            ]
+        )
+    ]
+)
